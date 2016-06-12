a2c58de5aedf9dc7a540a01c9efe6d87aadaca73
Mario Talevski <mario.talevski@gmail.com>
2016-06-12 14:21:23 +0200

    task

---
+++ fudbal/fudbal.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int best = 0, n;
+    cin >> n;
+
+    for (int i=0; i<n; i++)
+    {
+        int wi, di, li;
+        cin >> wi >> di >> li;
+
+        int points = 3*wi + di;
+
+        if (points > best)
+           best = points;
+    }
+
+    cout << best << endl;
+    return 0;
+}
